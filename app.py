5d29835588b896b874986b542331ee0ffcd2da30
sangoIPF <cdst1230403@gn.iwasaki.ed.jp>
2024-06-03 01:19:57 +0900

    flaskがhtmlを操作できることを確認。あと仕様ちょい書き足し

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+
 from flask import Flask,request,render_template
 from json import loads,dumps,load
+import datetime
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -9,7 +11,9 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route('/')
 def hello():
     if 'use_template' in request.args:
-        return render_template('index.html')
+        dt_now = datetime.datetime.now()
+        
+        return render_template('index.html', clockString=dt_now.strftime('%Y-%m-%d %H:%M:%S'))
     else:
-        return 'Hello World! to sango'
+        return 'Hello World! to the local file'
 
