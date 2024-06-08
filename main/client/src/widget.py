81c19f40abd3e301ef4786d816b6fed1cec1e3a7
sangoIPF <cdst1230403@gn.iwasaki.ed.jp>
2024-06-09 01:38:03 +0900

    title ok

--- main/client/src/widget.py
+++ main/client/src/widget.py
@@ -1,5 +1,28 @@
 import json, regex as re
 
+
+
+class sizedtext:
+    font_size = {
+        "s" : "0.5em",
+        "m" : "1em",
+        "l" : "2em",
+        "xl": "4em",
+        "xxl": "8em"
+    }
+    
+    def __init__(self, sizedtxt):
+        scale = re.match(r"^[^:]+(?=:)", sizedtxt)
+        text = sizedtxt[(0 if scale==None else len(scale.group())+1):]
+        if scale==None: scale = "m"
+        else: scale = scale.group()
+        
+        self.original = sizedtxt            # m:text
+        self.text = text                    # text
+        self.scale = scale                  # s, m, l, xl, xxl
+        self.font_size = self.font_size[scale]   # em
+        
+
 class widget:
     def __init__(self, widget_path, id):
         
@@ -11,12 +34,27 @@ class widget:
                 
     
     def create_widget(self):
+        widget_str = ""
         # title
+        _title = None
+        if (hasattr(self, "title") and self.title!="") :
+            _title = sizedtext(self.title)
+            _title.div =f'<div class="widget_title" \
+                style="font-size:{_title.font_size};">{_title.text}</div>'
         
         # sync
+        if hasattr(self, "sync") and self.sync != "none" :
+            ...
         
         # container
+        if not hasattr(self, "container") : self.container = "vbox"
+        widget_str+= '<div class="components">'
         
         # content
+        if hasattr(self, "content") :
+            for component in self.content:
+                ...
         
-        return f'<div class="widget" id="w{self.id}">TODO</div>'
+        widget_str += '</div>'
+        if _title: widget_str += _title.div
+        return f'<div class="widget" id="w{self.id}">{widget_str}</div>'
