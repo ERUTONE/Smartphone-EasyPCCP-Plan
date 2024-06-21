9adc5763285fe8dd650e63b4d3bb6c7b36edd3bc
KoralMint <aitaiyosora300@gmail.com>
2024-06-21 15:31:59 +0900

    js用意

--- main/functions/datetime.py
+++ main/functions/datetime.py
@@ -1,6 +1,9 @@
+import main.globals as g
 
 def time(format="hh:mm:ss"):
     # TODO
+    with open(g.functions+"datetime/time.js") as f:
+        ...
     return f"TIME {format}"
 
 def date(format="YYYY/MM/DD"):
